8b536a40066658a375e0ff76c182f33e7f8d6b6e
Gökhan Ünal <unal_gokhan@outlook.com>
2020-11-22 22:36:51 +0300

    add GameList component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,50 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { loadGames } from '../actions/gamesAction'
+import { Game } from '../components/Game'
+import styled from 'styled-components/macro'
+import { motion } from 'framer-motion'
 
 export const Home = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(loadGames())
-  })
+  }, [dispatch])
+
+  // const games = useSelector((state) => state.games)
+  // or easy way to extract data
+  const { popular, newGames, upcoming } = useSelector((state) => state.games)
 
   return (
-    <h1>Home</h1>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
   )
 }
+
+const GameList = styled(motion.div)`
+  padding: 0 5rem;
+  h2 {
+    padding: 5rem 0;
+  }
+`
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`
