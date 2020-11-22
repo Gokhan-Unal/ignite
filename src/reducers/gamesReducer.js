068e05b1f1ce35f2e2901b85b60821ada6ee876f
Gökhan Ünal <unal_gokhan@outlook.com>
2020-11-22 20:50:37 +0300

    add gamesReducer

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,13 +1,19 @@
 const initialState = {
   popular: [],
   newGames: [],
-  upComing: [],
+  upcoming: [],
+  searched: [],
 }
 
 const gameReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_GAMES':
-      return { ...state }
+      return {
+        ...state,
+        popular: action.payload.popular,
+        upcoming: action.payload.upcoming,
+        newGames: action.payload.newGames,
+      }
 
     default:
       return state
