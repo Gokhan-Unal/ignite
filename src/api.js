c575ec8aa7184d6de5378cee08ab8db7ecf6f8b3
Gökhan Ünal <unal_gokhan@outlook.com>
2020-11-22 20:51:42 +0300

    add api hit points

--- src/api.js
+++ src/api.js
@@ -1,11 +1,15 @@
-import { currentDate, lastYear } from './utils/date.js'
+import { currentDate, lastYear, nextYear } from './utils/date.js'
 
 // Base URL
 const baseUrl = 'https://api.rawg.io/api/'
 
 // Popular Games
 const popularGames = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`
+export const upcomingGames = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`
+const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`
 
 export const popularGamesURL = () => `${baseUrl}${popularGames}`
+export const upcomingGamesURL = () => `${baseUrl}${upcomingGames}`
+export const newGamesURL = () => `${baseUrl}${newGames}`
 
-console.log(popularGamesURL());
+// console.log(popularGamesURL())
