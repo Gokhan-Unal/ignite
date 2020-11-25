43ea341ed2ceb1251d990dd64673d7306aa39a84
Gökhan Ünal <unal_gokhan@outlook.com>
2020-11-25 18:50:34 +0300

    add a util function

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { motion } from 'framer-motion'
 import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
+import { smallImage } from '../util'
 
 const GameDetail = () => {
   const history = useHistory()
@@ -36,14 +37,21 @@ const GameDetail = () => {
               </Info>
             </Stats>
             <Media>
-              <img src={game.background_image} alt={game.name} />
+              <img
+                src={smallImage(game.background_image, 1280)}
+                alt={game.name}
+              />
             </Media>
             <Description>
               <p>{game.description_raw}</p>
             </Description>
             <div className="gallery">
               {screen.results.map((screen) => (
-                <img key={screen.id} src={screen.image} alt="Background" />
+                <img
+                  key={screen.id}
+                  src={smallImage(screen.image, 1280)}
+                  alt="Background"
+                />
               ))}
             </div>
           </Detail>
